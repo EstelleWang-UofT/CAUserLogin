d0668f280d48993b5d9e52380378edf27e9e82cc
estelleyay <wxiaoyi595@gmail.com>
2024-10-23 00:26:36 -0400

    new method setCurrentUser to keep track of which user is logged in now.

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Keep track of which user is logged in.
+     * @param name the name of the logged-in user
+     */
+    void setCurrentUser(String name);
 }
